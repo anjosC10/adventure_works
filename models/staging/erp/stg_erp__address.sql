11e6f6aa2691723c3884d727dcdc5c1422b46104
anjosC10 <lucasdosanjos.1999@gmail.com>
2024-10-10 17:48:39 -0300

    finish staging locations and yml

---
+++ models/staging/erp/stg_erp__address.sql
@@ -0,0 +1,22 @@
+with
+    raw_address as (
+        select *
+        from {{ source('erp_aw', 'address') }}
+    )
+
+    , renamed as (
+        select
+            cast(ADDRESSID as int) as pk_address
+            , cast(STATEPROVINCEID as int) fk_state_province
+            , cast(CITY as varchar) as city_address
+            --, cast(ADDRESSLINE1 as varchar)
+            --, cast(ADDRESSLINE2 as varchar)
+            --, cast(POSTALCODE as varchar)
+            --, cast(SPATIALLOCATION as varchar)
+            --, cast(ROWGUID as varchar)
+            --, cast(MODIFIEDDATE as varchar)
+        from raw_address
+    )
+
+select *
+from renamed
