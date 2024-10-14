414f11b76a2a8cf091f4513bc349df04356c4b30
anjosC10 <lucasdosanjos.1999@gmail.com>
2024-10-14 16:32:04 -0300

    fix pk_order name

--- models/staging/erp/stg_erp__salesorderheadersalesreasons.sql
+++ models/staging/erp/stg_erp__salesorderheadersalesreasons.sql
@@ -6,7 +6,7 @@ with
 
     , renamed as (
         select
-            cast(SALESORDERID as int) as pk_salesorder
+            cast(SALESORDERID as int) as pk_order
             , cast(SALESREASONID as int) as fk_reason
             --, cast(MODIFIEDDATE as varchar)
         from raw_salesorderheadersalesreason
