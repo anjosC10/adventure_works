7975162425d8a05a2e520b4728cf7dbb896dbe25
anjosC10 <lucasdosanjos.1999@gmail.com>
2024-10-14 10:31:10 -0300

    add aggregation in reasons

--- models/sales/intermediate/int_reasons.sql
+++ models/sales/intermediate/int_reasons.sql
@@ -20,5 +20,14 @@ with
             on salesorderheadersalesreasons.fk_reason = salesreasons.pk_reason
     )
 
+    , aggregated as (
+        select
+            pk_salesorder
+            , listagg(trim(reason_name), '; ') within group (order by reason_name) as reason_name
+            , listagg(trim(reason_type), '; ') within group (order by reason_type) as reason_type
+        from joined
+        group by pk_salesorder
+    )
+
 select *
-from joined
+from aggregated
