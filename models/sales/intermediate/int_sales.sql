684496f7c08ab74aa1be88365d8068c9abeccf67
anjosC10 <lucasdosanjos.1999@gmail.com>
2024-10-14 15:44:27 -0300

    add prorated and avg tickets measures

--- models/sales/intermediate/int_sales.sql
+++ models/sales/intermediate/int_sales.sql
@@ -42,9 +42,32 @@ with
             *
             , unit_price * quantity as gross_value
             , unit_price * quantity * (1-discount) as net_value
+            , order_freight / (count(*) over(partition by fk_order)) as prorated_freight
+            , order_tax_amount / (count(*) over(partition by fk_order)) as prorated_tax_amount
+            , (net_value + prorated_freight + prorated_tax_amount) as estimated_profit
         from joined
     )
 
+    , tickets as (
+        select
+            fk_order
+            , sum(net_value) / count(fk_order) as avg_order_ticket_product
+            , sum(estimated_profit) / count(fk_order) as avg_order_ticket_total
+        from metrics
+        group by fk_order
+    )
+
+    , rejoined as (
+        select
+            metrics.*
+            , tickets.avg_order_ticket_product
+            , tickets.avg_order_ticket_total
+        from metrics
+        left join tickets
+            on metrics.fk_order = tickets.fk_order
+    )
+
+    
     , reorder as (
         select
             PK_ORDERDETAIL
@@ -59,17 +82,18 @@ with
             , ORDER_SHIP_DATE
             , ORDER_STATUS
             , STATUS_DESCRIPTION
-            , ORDER_SUBTOTAL
-            , ORDER_TAX_AMOUNT
-            , ORDER_FREIGHT
-            , ORDER_TOTAL
             , QUANTITY
             , UNIT_PRICE
             , DISCOUNT
             , GROSS_VALUE
             , NET_VALUE
+            , PRORATED_FREIGHT
+            , PRORATED_TAX_AMOUNT
+            , ESTIMATED_PROFIT
+            , AVG_ORDER_TICKET_PRODUCT
+            , AVG_ORDER_TICKET_TOTAL
             , IS_ONLINEORDER
-        from metrics
+        from rejoined
     )
 
 select *
