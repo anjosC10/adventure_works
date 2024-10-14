794d541c41a889c30a521d6f31a1cebcdf22fef9
anjosC10 <lucasdosanjos.1999@gmail.com>
2024-10-14 16:35:29 -0300

    fix pk_order name

--- models/sales/intermediate/int_reasons.sql
+++ models/sales/intermediate/int_reasons.sql
@@ -11,7 +11,7 @@ with
 
     , joined as (
         select
-            salesorderheadersalesreasons.pk_salesorder
+            salesorderheadersalesreasons.pk_order
             , salesorderheadersalesreasons.fk_reason
             , salesreasons.reason_name
             , salesreasons.reason_type
@@ -22,11 +22,11 @@ with
 
     , aggregated as (
         select
-            pk_salesorder
+            pk_order
             , listagg(trim(reason_name), '; ') within group (order by reason_name) as reason_name
             , listagg(trim(reason_type), '; ') within group (order by reason_type) as reason_type
         from joined
-        group by pk_salesorder
+        group by pk_order
     )
 
 select *
