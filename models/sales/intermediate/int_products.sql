27655fa5f2c4f1950b8e59cb29706c763d65c17d
anjosC10 <lucasdosanjos.1999@gmail.com>
2024-10-13 21:52:36 -0300

    finish categories and subcategories stagings

--- models/sales/intermediate/int_products.sql
+++ models/sales/intermediate/int_products.sql
@@ -1,8 +1,33 @@
 with
-    int_products as (
+    products as (
         select *
         from {{ ref('stg_erp__products') }}
     )
 
+    , subcategories as (
+        select *
+        from {{ ref('stg_erp__productsubcategories') }}
+    )
+
+    , categories as (
+        select *
+        from {{ ref('stg_erp__productcategories') }}
+    )
+
+    , joined as (
+        select
+            products.pk_product
+            , categories.pk_product_category as fk_product_category
+            , products.fk_product_subcategory
+            , products.product_name
+            , categories.product_category
+            , subcategories.product_subcategory
+        from products
+        left join subcategories
+            on products.fk_product_subcategory = subcategories.pk_product_subcategory
+        left join categories
+            on subcategories.fk_product_category = categories.pk_product_category
+    )
+
 select *
-from int_products
+from joined
