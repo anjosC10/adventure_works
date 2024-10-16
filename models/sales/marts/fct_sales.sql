05da00d4c1d00909e92b060048fbf0e06227f5b9
anjosC10 <lucaswsl@SamsungBookLA>
2024-10-16 15:32:59 -0300

    add dim relationships for lineage purposes

--- models/sales/marts/fct_sales.sql
+++ models/sales/marts/fct_sales.sql
@@ -1,5 +1,35 @@
 with
-    final_sales as (
+    dim_creditcards as (
+        select *
+        from {{ ref('dim_creditcards') }}
+    )
+    
+    , dim_customers as (
+        select *
+        from {{ ref('dim_customers') }}
+    )
+    
+    , dim_dates as (
+        select *
+        from {{ ref('dim_dates') }}
+    )
+
+    , dim_locations as (
+        select *
+        from {{ ref('dim_locations') }}
+    )
+    
+    , dim_products as (
+        select *
+        from {{ ref('dim_products') }}
+    )
+
+    , dim_reasons as (
+        select *
+        from {{ ref('dim_reasons') }}
+    )
+    
+    , final_sales as (
         select *
         from {{ ref('int_sales') }}
     )
