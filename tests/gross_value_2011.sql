44385d305364024cddb981cb82f12c9d68d034d0
anjosC10 <lucaswsl@SamsungBookLA>
2024-10-16 15:34:17 -0300

    rewrite in cte form for codestyle

--- tests/gross_value_2011.sql
+++ tests/gross_value_2011.sql
@@ -1,9 +1,16 @@
 /* This test verifies that the total gross value for the year 2011 matches the expected
-reference value ($12.646.112,16), with a small margin of error to account for floating-point
-precision issues. */
+reference value given by Carlos ($12.646.112,16), with a small margin of error to account
+for floating-point precision issues. */
+
+with
+    gross_value_2011 as(
+        select
+            sum(gross_value) as total_gross_value
+        from {{ ref('int_sales') }}
+        where year(order_date) = 2011
+    )
 
 select
-    sum(gross_value) as total_gross_value
-from {{ ref('fct_sales') }}
-where year(order_date) = 2011
-having abs(total_gross_value - 12646112.16) > 0.05
+    total_gross_value
+from gross_value_2011
+where total_gross_value not between 12646112.16 and 12646112.26
